682ca5e00d43fac6fff66ef4c77c06abd44962f3
lyusinoel <seungin.lyu@students.olin.edu>
2016-12-03 15:22:05 -0500

    magnus_first
    added magnus force to the flow function

--- tennisball_flows.m
+++ tennisball_flows.m
@@ -1,21 +1,55 @@
 function res = tennisball_flows(~, Q)
     %CONSTANTS
     density_air = 1.225; %kg/m^3
-    drag_coef = 0.5; %dimensionless
-    area = 0.0335; %m^2 (cross-sectional area of ball; "how much air do I have to push out of the way?")
+    
+    area = 0.0335; %m^2 (cross-sectional area of ball; ...
+    %"how much air do I have to push out of the way?")
+    
     mass_ball = 0.0585; %kg
     gravity = 9.8; %m/s^2
-
+   
     x = Q(1);
     y = Q(2);
     vx = Q(3);
     vy = Q(4);
+    
+    rpm = Q(5);
+    %drag & lift coefficient depends on RPM * velocity
+    velocity = sqrt(vx^2 + vy^2);    
+    
+    S = rpm * velocity;
+    drag_coef = -4e-7*S + 0.5605; %dimensionless
+    
+    if(rpm > 0)
+    lift_coef = -(3e-7*S + 0.1086)
+    elseif (rpm == 0)
+    lift_coef = 0;
+    else
+    lift_coef = (3e-7*S + 0.1086)
+    end
+    
+    
 
     %DIFFERENTIAL EQUATIONS
     deriv_x = vx;
     deriv_y = vy;
-    deriv_vx = (-0.5 * density_air * drag_coef * area)/ (mass_ball) * (vx^2 + vy^2) * vx / (sqrt(vx^2 + vy^2));
-    deriv_vy = -gravity - ((    0.5 * density_air * drag_coef * area * (vx^2 + vy^2) * vy / (sqrt(vx^2 + vy^2)))) / mass_ball;
-
-    res = [deriv_x ; deriv_y ; deriv_vx ; deriv_vy];
+   
+    %X-component(acceleration)
+    drag_x = (-0.5 * density_air * drag_coef * area)... 
+        * (vx^2 + vy^2) * vx / (sqrt(vx^2 + vy^2));
+    lift_x = (-0.5 * density_air * lift_coef * area)... 
+        * (vx^2 + vy^2) * vy / (sqrt(vx^2 + vy^2));
+    
+    deriv_vx = (drag_x + lift_x)/mass_ball;
+    
+    %Y-component(acclearation)
+    gravity_y = -mass_ball * gravity;
+    drag_y = -((0.5 * density_air * drag_coef * area * ...
+        (vx^2 + vy^2) * vy / (sqrt(vx^2 + vy^2))));
+    lift_y = ((0.5 * density_air * lift_coef * area * ...
+        (vx^2 + vy^2) * vx / (sqrt(vx^2 + vy^2))));
+    
+    deriv_vy = (gravity_y + drag_y + lift_y)/mass_ball; 
+    
+    res = [deriv_x ; deriv_y ; deriv_vx ; deriv_vy; rpm];
 end
